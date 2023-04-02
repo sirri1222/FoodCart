e1727aeb27cd4854c981a5d6b06a95245655fbeb
sirri1222 <hyeonji159753@gmail.com>
2023-04-02 22:05:31 +0900

    메인 페이지 작업 진행중

--- src/component/Layout/Header.js
+++ src/component/Layout/Header.js
@@ -1,16 +1,19 @@
 import React from 'react'
-
+import mealsImg from '../../assets/meals.jpg'
+import classes from  './Header.module.css'
+import HeaderCartButton from './HeaderCartButton'
 const Header = () => {
     return (
         <React.Fragment>
 
-            <header>
-                <h1>ReactMeals</h1>
-                <button>Cart</button>
+            <header className={classes.header}>
+                <h1>delivery Yo!</h1>
+              <HeaderCartButton/>
             </header>
-            <div>
+            <div className={classes['main-image']}>
 
-                <img src='' alt='visual'></img>
+
+                <img src={mealsImg} alt='visual'></img>
             </div>
         </React.Fragment>
     )
---
+++ src/component/Meals/MealItem/MealItem.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const MealItem = props => {
+
+    const price = '${props.price.toFixed(2)}'
+  return (
+
+    <li>
+<div>
+    <h3>{props.name}</h3>
+    <div>{props.description}</div>
+    <div>{price}</div>
+</div>
+    <div>
+
+
+        
+    </div>
+
+    </li>
+   
+  )
+}
