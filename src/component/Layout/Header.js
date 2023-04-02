6483a78e566d0417ee4449f0040206e64c1580c2
sirri1222 <hyeonji159753@gmail.com>
2023-04-02 21:35:33 +0900

    header 진행중

---
+++ src/component/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <React.Fragment>
+
+            <header>
+                <h1>ReactMeals</h1>
+                <button>Cart</button>
+            </header>
+            <div>
+
+                <img src='' alt='visual'></img>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Header;
